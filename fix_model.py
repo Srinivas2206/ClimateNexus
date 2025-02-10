edbbc494243f51a61d80bbbd5ab49636160f2606
Srinivas Mandula <163158715+Srinivas2206@users.noreply.github.com>
2025-02-10 11:22:39 +0530

    Resolved .gitignore conflict

--- fix_model.py
+++
@@ -1,8 +0,0 @@
-from tensorflow.keras.models import load_model
-
-# Load model without compiling
-model = load_model("models/co2_lstm.h5", compile=False)
-
-# Save the model again to avoid serialization issues
-model.save("models/co2_lstm_fixed.h5")
-print("✅ Model saved successfully without compilation!")
